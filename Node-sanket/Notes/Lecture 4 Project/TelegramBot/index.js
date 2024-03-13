bd3cd7147f6634f635f63a8fba3791e26b483b26
AryanJindal <jindalaryan070707@gmail.com>
2024-03-13 12:36:15 +0530

    Token removal;

--- Node-sanket/Notes/Lecture 4 Project/TelegramBot/index.js
+++ Node-sanket/Notes/Lecture 4 Project/TelegramBot/index.js
@@ -1,6 +1,10 @@
 const {Telegraf} = require('telegraf');
+require('dotenv').config();
+
+const Token = process.env.TELEGRAM_TOKEN;
+// console.log(Token)
 // https://t.me/TheTechyBot
-const bot = new Telegraf('6784586591:AAFJ5URVLcgdrclK4hqD7Vs-BjPMeiwfI50');
+const bot = new Telegraf(Token);
 
 bot.start((ctx)=>ctx.reply('A bot by Aryan'));
 //command of hey set to the bot 
